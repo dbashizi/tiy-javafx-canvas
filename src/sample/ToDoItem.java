8cc2fc13914bb93176347749cedba1dffcd89f89
Daniel Nicodemo <dan.nicodemo@gmail.com>
2016-05-12 15:09:11 -0400

    added some functionality and unit testing

--- src/sample/ToDoItem.java
+++ src/sample/ToDoItem.java
@@ -6,10 +6,12 @@ package sample;
 public class ToDoItem {
     public String text;
     public boolean isDone;
+    public int id;
 
-    public ToDoItem(String text) {
+    public ToDoItem(int id, String text, boolean isDone) {
+        this.id = id;
         this.text = text;
-        this.isDone = false;
+        this.isDone = isDone;
     }
 
     public ToDoItem() {
