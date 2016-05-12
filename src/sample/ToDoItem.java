27ce8cbdacab07ca2a622f40bf626ea051f1babd
Evan <evanthomashicks@gmail.com>
2016-05-12 16:32:28 -0700

    Changes made testng commits with the class

--- src/sample/ToDoItem.java
+++ src/sample/ToDoItem.java
@@ -6,14 +6,19 @@ package sample;
 public class ToDoItem {
     public String text;
     public boolean isDone;
+    public int id;
 
     public ToDoItem(String text) {
         this.text = text;
         this.isDone = false;
     }
-
-    public ToDoItem() {
+    public ToDoItem(int id, String text, boolean isDone) {
+        this.id = id;
+        this.text = text;
+        this.isDone = isDone;
     }
+
+
 
     @Override
     public String toString() {
